97c505236fae8d25eccf2c80e4377ba3af10aed1
LuisPRG4 <lsalazar.8124@gmail.com>
2025-07-07 13:06:30 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Archivo: service-worker.js
 
 // Incrementamos la versión y agregamos timestamp para forzar actualización
-const CACHE_NAME = "los-ss-cache-v10";
+const CACHE_NAME = "los-ss-cache-v11";
 const APP_VERSION = "1.0.5"; // Incrementa esto cuando hagas cambios importantes
 
 // Función para determinar la ruta base según el entorno
